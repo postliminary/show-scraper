6a3e52ce1c180b3e970a0d53eb3cfbf0e7256b91
Thomas Tuttle <thomas@postliminary.com>
2014-07-10 10:44:44 +0900

    Tweak to loader airday mapping

--- show_scraper/loaders.py
+++ show_scraper/loaders.py
@@ -1,3 +1,4 @@
+import time
 from scrapy.contrib.loader import ItemLoader
 from scrapy.contrib.loader.processor import TakeFirst, MapCompose, Join, Identity
 
@@ -8,9 +9,9 @@ def get_airday(text):
     if (text.find("tuesday") > -1):
         return "tue"
     if (text.find("wednesday") > -1):
-        return "wednes"
+        return "wed"
     if (text.find("thursday") > -1):
-        return "thurs"
+        return "thr"
     if (text.find("friday") > -1):
         return "fri"
     if (text.find("saturday") > -1):
